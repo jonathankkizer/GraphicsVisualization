60eedde4d986dc04a9431f6427a17093e21ded10
jonathankkizer <jonathankkizer@gmail.com>
2018-02-06 12:11:29 -0600

    Extract all words, unique words

--- Homework 3/extract_words.py
+++ Homework 3/extract_words.py
@@ -1,4 +1,28 @@
-def main():
+# reads all words from filename, returns based on lin
+def readAllWords(filename):
+	with open(filename) as book:
+		return book.read().split()
+
+# opens a text file named "allWords.txt" and writes the contents of readAllWords() to a new line
+def writeAllWords(filename):
+	allWords = readAllWords(filename)
+	textFile = open("allWords.txt", "w")
+	for i in allWords:
+		i = i + "\n"
+		textFile.write(i)
+	textFile.close()
 	
+def uniqueWords():
+	with open("allWords.txt") as allWords:
+		uniqueWords = set(allWords)
+		textFile = open("uniqueWords.txt", "w")
+		for i in uniqueWords:
+			i = i + "\n"
+			textFile.write(i)
+		textFile.close()
+
+def main():
+	writeAllWords("TheInvisibleMan.txt")
+	uniqueWords()
 
 main()
